88b92bd9c32ab6dbe21a25e5ccf818e8ad0b1863
Michelle <earlycapistran@gmail.com>
2024-12-05 12:47:47 -0600

    Changed order of params and added example

--- R/save_ggplot.R
+++ R/save_ggplot.R
@@ -11,9 +11,19 @@
 #' @return saves ggplot in chosen location
 #'
 #'@export
+#'
+#'@examples
+#'# Create a simple ggplot object
+#'plot_object <- ggplot(mtcars, aes(wt, mpg)) + geom_point()
+#'# Define a filename with .svg extension
+#'filename <- "example_plot.svg"
+#'# Use a temporary directory or specify a path
+#'path <- tempdir()  # Using temp directory for simplicity
+#'# Call the function to save the plot
+#' save_ggplot(filename, plot_object, path)
 
-save_ggplot <- function(filename,
-                        plot_object,
+save_ggplot <- function(plot_object,
+                        filename,
                         path,
                         width = 20,
                         height = 20,
@@ -21,8 +31,8 @@ save_ggplot <- function(filename,
                         dpi = 300) {
 
   # Save the boxplot as an image file
-  ggsave(filename,
-         plot_object,
+  ggsave(plot_object,
+         filename,
          width = width,
          height = height,
          units = units,
