af5e8503f759e03d6d4968a68f9cb696594ddda2
Michelle <earlycapistran@gmail.com>
2024-12-05 12:10:22 -0600

    Rxoygenized

--- R/save_ggplot.R
+++ R/save_ggplot.R
@@ -1,21 +1,32 @@
-# Saves ggplot object with defined parameters
+# =============================================================================
+# Save ggplot
+# earlyc@stanford.edu - December, 2024
+# =============================================================================
 
-save_ggplot <- function(filename, 
+#' Saves ggplot objects as .svg at a high resolution
+#'
+#' @param filename
+#' @param plot_object
+#' @param path
+#' @return saves ggplot in chosen location
+#'
+
+save_ggplot <- function(filename,
                         plot_object,
                         path,
-                        width = 20, 
-                        height = 20, 
-                        units = "cm", 
+                        width = 20,
+                        height = 20,
+                        units = "cm",
                         dpi = 300) {
   # Load the ggplot2 library
   library(ggplot2)
-  
+
   # Save the boxplot as an image file
-  ggsave(filename, 
-         plot_object, 
-         width = width, 
-         height = height, 
-         units = units, 
+  ggsave(filename,
+         plot_object,
+         width = width,
+         height = height,
+         units = units,
          dpi = dpi,
          path = path)
 }
